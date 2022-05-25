72e1e9d23c4ee2776d06f9bfc8ba9b584784f7ab
Cris <c.fernandez.melian@hotmail.com>
2022-05-25 12:57:20 -0300

    Agregue Ruteo Home, Lista de Peliculas e Informacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { InfoPeliculasComponent } from './info-peliculas/info-peliculas.component';
 import { ListaDePeliculasComponent } from './lista-de-peliculas/lista-de-peliculas.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent},//esta
   { path: 'peliculas', component: ListaDePeliculasComponent },//esta
+  { path: 'info/:id', component: InfoPeliculasComponent },
+
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaDePeliculasComponent } from './lista-de-peliculas/lista-de-peliculas.component';
+import { HomeComponent } from './home/home.component';
+import { InfoPeliculasComponent } from './info-peliculas/info-peliculas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListaDePeliculasComponent
+    ListaDePeliculasComponent,
+    HomeComponent,
+    InfoPeliculasComponent
   ],
   imports: [
     BrowserModule,
