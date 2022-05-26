10ab53e898dc1c981b7eec4983a3a08298a1e08a
julieta96 <julietaextra14@gmail.com>
2022-05-26 17:51:41 -0300

    agregue un array de peliculas

--- src/app/components/lista-de-peliculas/lista-de-peliculas.component.ts
+++ src/app/components/lista-de-peliculas/lista-de-peliculas.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  //selector:'app-peliculas', 
   templateUrl: './lista-de-peliculas.component.html',
   styleUrls: ['./lista-de-peliculas.component.css']
 })
@@ -8,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 
 export class ListaDePeliculasComponent implements OnInit {
 
+  public peliculas:Array<any> = [];
+  public descripcion:string = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.";
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.peliculas=[
+      {
+        id:1,
+        titulo: "Sony",
+        imagen:"./assets/img/peliculas/sonic.jpg",
+        descripcion:"Va a los chapazos"
+      },
+      {
+        id:2,
+        titulo: "Joker",
+        imagen:"./assets/img/peliculas/joker.jpg",
+        descripcion:this.descripcion
+      },
+      {
+        id:3,
+        titulo: "Gremlins",
+        imagen:"./assets/img/peliculas/gremlins.jpg",
+        descripcion:this.descripcion
+      },
+      {
+        id:4,
+        titulo: "Et",
+        imagen:"./assets/img/peliculas/et.jpg",
+        descripcion:this.descripcion
+      },
+      {
+        id:5,
+        titulo: "Harry Potter",
+        imagen:"./assets/img/peliculas/harry-potter.jpg",
+        descripcion:this.descripcion
+      }
+    ]
   }
 
 }
