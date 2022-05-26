ffc54112cefb7e3c4330f396f4fd9d96fcb2ef18
Ivan Kek <ivan.kek@hotmail.com>
2022-05-26 14:43:59 -0300

    agregando screen login y registro, falta mejorar registro

--- src/app/components/authentication/authentication.component.ts
+++ src/app/components/authentication/authentication.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-authentication',
   templateUrl: './authentication.component.html',
-  styleUrls: ['./authentication.component.css']
+  styleUrls: ['./authentication.component.css'],
 })
 export class AuthenticationComponent implements OnInit {
+  public screen: number;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.screen = 1;
   }
 
+  ngOnInit(): void {}
+
+  changeScreen() {
+    this.screen = this.screen === 1 ? 2 : 1;
+  }
 }
