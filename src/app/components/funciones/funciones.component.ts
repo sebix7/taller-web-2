79ca2d747dc8183c6c5b6ec3765df321e59a1573
Cris <c.fernandez.melian@hotmail.com>
2022-06-03 16:02:33 -0300

    sqlite

--- src/app/components/funciones/funciones.component.ts
+++ src/app/components/funciones/funciones.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-funciones',
   templateUrl: './funciones.component.html',
   styleUrls: ['./funciones.component.css']
 })
