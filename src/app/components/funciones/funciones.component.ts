1c92a370098e7252599348a6ade270441f4a8c4b
Cris <c.fernandez.melian@hotmail.com>
2022-06-03 18:04:21 -0300

    Intento router-outlet no sale

--- src/app/components/funciones/funciones.component.ts
+++ src/app/components/funciones/funciones.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  selector: 'app-funciones',
+
   templateUrl: './funciones.component.html',
   styleUrls: ['./funciones.component.css']
 })
