ea4c760de42bcf11f33231102d1351b58ebdb186
Ivan Kek <ivan.kek@hotmail.com>
2022-06-11 02:20:21 -0300

    Agregando ruteo backend

--- src/app/components/info-peliculas/info-peliculas.component.ts
+++ src/app/components/info-peliculas/info-peliculas.component.ts
@@ -1,18 +1,21 @@
 import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpErrorResponse, JsonpClientBackend } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+  JsonpClientBackend,
+} from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { share, catchError } from 'rxjs/operators';
 import { Pelicula } from '../home/pelicula';
 
-
 @Component({
   selector: 'app-info-peliculas',
   templateUrl: './info-peliculas.component.html',
-  styleUrls: ['./info-peliculas.component.css']
+  styleUrls: ['./info-peliculas.component.css'],
 })
 export class InfoPeliculasComponent implements OnInit {
-
   pelicula: Pelicula = {
     id: 0,
     titulo: '',
@@ -23,40 +26,37 @@ export class InfoPeliculasComponent implements OnInit {
     actores: '',
     director: '',
     trailer: '',
-    estreno:true,
+    estreno: true,
   };
 
   Peliculas: any;
-  idPelicula:any;
-  constructor(protected router: Router, protected httpClient: HttpClient,protected route: ActivatedRoute) {
-
-  }
+  idPelicula: any;
+  constructor(
+    protected router: Router,
+    protected httpClient: HttpClient,
+    protected route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    const tag = document.createElement('script')
+    const tag = document.createElement('script');
     tag.src = 'https://www.youtube.com/iframe_api';
     document.body.appendChild(tag);
 
-    this.idPelicula = this.route.snapshot.paramMap.get("id");
+    this.idPelicula = this.route.snapshot.paramMap.get('id');
 
-    let res: Observable<Pelicula[]> =
-     this.httpClient.get<Pelicula[]>(`http://localhost:3000/${this.idPelicula}`)
-     .pipe(share());
+    let res: Observable<Pelicula[]> = this.httpClient
+      .get<Pelicula[]>(`http://localhost:3000/peliculas/${this.idPelicula}`)
+      .pipe(share());
 
-     res.subscribe( 
-
-      value => { 
-         console.log(value)
-         this.Peliculas = value[0];
-         this.pelicula = this.Peliculas;
-
+    res.subscribe(
+      (value) => {
+        console.log(value);
+        this.Peliculas = value[0];
+        this.pelicula = this.Peliculas;
       },
-      error => {
+      (error) => {
         console.log('ocurrio un error');
-      });
-
-      
-  
+      }
+    );
   }
-
 }
--- src/app/components/lista-de-peliculas/lista-de-peliculas.component.ts
+++ src/app/components/lista-de-peliculas/lista-de-peliculas.component.ts
@@ -5,41 +5,29 @@ import { Observable, share } from 'rxjs';
 import { Pelicula } from '../home/pelicula';
 
 @Component({
-  selector:'app-peliculas', 
+  selector: 'app-peliculas',
   templateUrl: './lista-de-peliculas.component.html',
-  styleUrls: ['./lista-de-peliculas.component.css']
+  styleUrls: ['./lista-de-peliculas.component.css'],
 })
-
-
 export class ListaDePeliculasComponent implements OnInit {
-
   peliculas: Pelicula[] = [];
   Peliculas: any;
 
+  constructor(protected router: Router, protected httpClient: HttpClient) {}
 
-  constructor(protected router: Router, protected httpClient: HttpClient) {
+  ngOnInit(): void {
+    let res: Observable<Pelicula[]> = this.httpClient
+      .get<Pelicula[]>('http://localhost:3000/peliculas')
+      .pipe(share());
 
-  }
-
-   ngOnInit(): void {
-    let res: Observable<Pelicula[]> =
-     this.httpClient.get<Pelicula[]>('http://localhost:3000/')
-     .pipe(share());
-
-     res.subscribe( 
-
-      value => { 
-
-         this.Peliculas = value;
-         this.peliculas = this.Peliculas;
-
+    res.subscribe(
+      (value) => {
+        this.Peliculas = value;
+        this.peliculas = this.Peliculas;
       },
-      error => {
+      (error) => {
         console.log('ocurrio un error');
-      });
-
-      
+      }
+    );
   }
-
-
 }
