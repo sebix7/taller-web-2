59536ef1b980f53d63dd67f53d3dddc1d94cdd20
julieta96 <julietaextra14@gmail.com>
2022-06-12 04:54:46 -0300

    se agrego vista reserva

--- src/app/components/reserva/reserva.component.ts
+++ src/app/components/reserva/reserva.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, share } from 'rxjs';
+//import { Pelicula } from '../home/pelicula';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-reserva',
@@ -14,10 +18,26 @@ export class ReservaComponent implements OnInit {
   duracion: string;
   actores: string;
   director: string;
+  //pelicula:Pelicula;
 
-  constructor() {
+  constructor( private route:ActivatedRoute,protected httpClient: HttpClient) {
+    /*this.id=this.route.snapshot.params['id'];
+    let res: Observable<Pelicula[]> = this.httpClient
+      .get<Pelicula[]>(`http://localhost:3000/peliculas/${this.id}`)
+      .pipe(share());
+
+    res.subscribe(
+      (value) => {
+        console.log(value);
+        this.pelicula = value[0];
+      },
+      (error) => {
+        console.log('ocurrio un error');
+      }
+    );*/
+    
     (this.id = 2),
-      (this.titulo = 'Joker'),
+      (this.titulo = "Joker"),
       (this.imagen = './assets/img/peliculas/joker.jpg'),
       (this.descripcion =
         'Joker mostrará por primera vez los orígenes del icónico archienemigo por excelencia de Bruce Wayne/Batman. La historia sigue de cerca la vida de Arthur Fleck (Joaquin Phoenix), un hombre con problemas psiquiátricos que vivirá una serie de acontecimientos que le harán convertirse en uno de los grandes villanos de DC Comics. El Príncipe Payaso del Crimen se cruzará en el camino de Thomas Wayne (Brett Cullen) y se acercará a su hijo, el futuro Caballero Oscuro en su versión joven (Dante Pereira-Olson).'),
